31b88ba1681db7c510018b8f1153a35764ffe4fd
nabilweq <nabeeltkanr@gmail.com>
2023-03-13 20:56:08 +0530

    cors

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(xss());
 app.use(mongoSanitize());
 app.use(morgan("tiny"));
 
-const whitelist = ["http://127.0.0.1:3000", "localhost", "http://localhost:3000"];
+const whitelist = ["http://127.0.0.1:3000", "localhost", "http://localhost:3000", "https://pdf-vishnu.vercel.app"];
 
 const corsOptions = {
 	// eslint-disable-next-line consistent-return
